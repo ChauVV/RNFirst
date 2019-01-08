f0a8e25fa7ce2823b2039ed7e503a283647ed556
gvl <>
2019-01-08 11:50:10 +0700

    init

---
+++ src/Frontend/Screens/Setting/index.js
@@ -0,0 +1,29 @@
+import BaseView from 'frontend/Containers/BaseView'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default class Setting extends Component {
+  render () {
+    return (
+      <BaseView>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native B</Text>
+        </View>
+      </BaseView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
